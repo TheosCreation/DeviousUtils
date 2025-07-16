0f3a9bcc9bac4b1ebd5345c1c2dde88895ad6a11
Theo Morris <83748550+TheosCreation@users.noreply.github.com>
2025-07-16 23:34:03 +1200

    Update DeviousUtils.cs

--- Runtime/DeviousUtils.cs
+++ Runtime/DeviousUtils.cs
@@ -4,6 +4,10 @@ namespace DeviousDevs
 {
     public static class DeviousUtils
     {
-
+        // Layer mask checking if a layer is inside the mask
+        public static bool Contains(this LayerMask mask, int layer)
+        {
+            return (mask & (1 << layer)) != 0;
+        }
     }
 }
